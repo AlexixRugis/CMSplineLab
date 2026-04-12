9cba98916d53e372ce61f2ed83c5f72fc0e65751
Леонид Гонозов <leogonz2005@mail.ru>
2026-04-12 20:47:12 +0300

    Added functions

---
+++ code/Graphics-ZedGraph-master/Functions.hpp
@@ -0,0 +1,25 @@
+#include "Common.hpp"
+#include <cmath>
+
+using namespace std;
+
+namespace CMSpline {
+
+    fp test_function(fp x) {
+        if (x >= -1 && x <= 0) {
+            return x * x * x + 3 * x * x;
+        }
+        else if (x <= 1 && x > 0) {
+            return - (x * x * x) + 3 * x * x;
+        }
+        return NAN;
+    }
+
+    fp main_function(fp x) {
+        return sin(x + 1) / (x + 1);
+    }
+
+    fp oscillating_function(fp x) {
+        return sin(x + 1) / (x + 1) + cos(10);
+    }
+}
