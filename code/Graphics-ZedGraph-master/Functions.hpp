f7bd253cd593ba7d5ff107005d3b4b57e8d14dea
Леонид Гонозов <leogonz2005@mail.ru>
2026-04-12 22:17:01 +0300

    Added derivatives of functions

--- code/Graphics-ZedGraph-master/Functions.hpp
+++ code/Graphics-ZedGraph-master/Functions.hpp
@@ -15,11 +15,47 @@ namespace CMSpline {
         return NAN;
     }
 
+    fp test_function_derivative1(fp x) {
+        if (x >= -1 && x <= 0) {
+            return 3 * x * x + 6 * x;
+        }
+        else if (x <= 1 && x > 0) {
+            return -3 * x * x + 6 * x;
+        }
+        return NAN;
+    }
+
+    fp test_function_derivative2(fp x) {
+        if (x >= -1 && x <= 0) {
+            return 6 * x + 6;
+        }
+        else if (x <= 1 && x > 0) {
+            return -6 * x + 6;
+        }
+        return NAN;
+    }
+
     fp main_function(fp x) {
         return sin(x + 1) / (x + 1);
     }
 
+    fp main_function_derivative1(fp x) {
+        return -(sin(x + 1) + (-x-1) * cos(x+1)) / (x * x + 2 * x + 1);
+    }
+
+    fp main_function_derivative2(fp x) {
+        return -((x * x + 2 * x + 1) * sin(x + 1) + 2 * (x + 1) * cos(x + 1)) / (x * x * x + 3 * x * x + 3 * x + 1);
+    }
+
     fp oscillating_function(fp x) {
         return sin(x + 1) / (x + 1) + cos(10 * x);
+    }
+
+    fp oscillating_function_derivative1(fp x) {
+        return -(sin(x + 1) + (-x - 1) * cos(x + 1)) / (x * x + 2 * x + 1) - 10 * sin(10 * x);
+    }
+
+    fp oscillating_function_derivative2(fp x) {
+        return -((x * x + 2 * x + 1) * sin(x + 1) + 2 * (x + 1) * cos(x + 1)) / (x * x * x + 3 * x * x + 3 * x + 1) - 100 * cos(10 * x);
     }
 }
